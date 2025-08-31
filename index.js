e9458d9e3a1697311c9276ac639a5d9ecc46d9f8
Shivam Gupta <shivamgupta1700000@gmail.com>
2025-08-31 21:10:23 +0530

    ldjlkj:

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/v1/purchase/webhook", purchaseRoute);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:["http://localhost:3000","https://e-learning-client-sigma.vercel.app"],
     credentials:true,
 }));
 
